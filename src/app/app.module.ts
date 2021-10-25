b57ee99719faba79a5c8882d0d4e286acde9a7db
JonathanArenth <arenth.jonathan@gmail.com>
2021-10-25 16:48:49 +0200

    architecture project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/container/home-page/home-page.component';
+import { ContainerComponent } from './pages/container/container.component';
+import { TopbarComponent } from './component/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContainerComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule
